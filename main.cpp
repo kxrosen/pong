c9edba605f6b6538d32bdea96ca7f53933f3c132
kxrosen <alxjbecg@gmail.com>
2025-04-10 17:06:24 +0700

    added pong

--- main.cpp
+++ main.cpp
@@ -1,16 +1,32 @@
 #include "include/raylib.h"
 
+#include "global.hpp"
+#include "pong.hpp"
+
 int main()
 {
-  InitWindow(100, 100, "test");
+  InitWindow(WINDOW_WIDTH, WINDOW_HEIGHT, WINDOW_TITLE);
+  SetTargetFPS(WINDOW_FPS);
+
+  pong pong;
+  pong.radius = 20;
+  pong.pos.x = WINDOW_WIDTH / 2 - pong.radius / 2;
+  pong.pos.y = WINDOW_HEIGHT / 2 - pong.radius / 2;
+  pong.speedx = 500;
+  pong.speedy = 100;
+  pong.color = WHITE;
 
   while (!WindowShouldClose())
   {
+    pong.updatepos();
+    pong.bordervertical();
+
     BeginDrawing();
     ClearBackground(BLUE);
 
-    DrawText("I'M WORKING", 0, 0, 20, WHITE);
+    pong.draw();
 
+    DrawFPS(0, 0);
     EndDrawing();
   }
 
