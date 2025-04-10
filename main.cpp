eb22183f3fb9b368514e74ac8de6074295ee7bf0
kxrosen <alxjbecg@gmail.com>
2025-04-10 17:20:49 +0700

    halfay implementing player

--- main.cpp
+++ main.cpp
@@ -1,7 +1,12 @@
+
+// todo: make the player better
+
+
 #include "include/raylib.h"
 
 #include "global.hpp"
 #include "pong.hpp"
+#include "player.hpp"
 
 int main()
 {
@@ -16,15 +21,26 @@ int main()
   pong.speedy = 100;
   pong.color = WHITE;
 
+  player player1;
+  player1.posz.width = 10;
+  player1.posz.height = 100;
+  player1.speedy = 500;
+  player1.color = WHITE;
+  player1.keyup = KEY_W;
+  player1.keydown = KEY_S;
+
   while (!WindowShouldClose())
   {
     pong.updatepos();
     pong.bordervertical();
 
+    player1.input();
+
     BeginDrawing();
     ClearBackground(BLUE);
 
     pong.draw();
+    player1.draw();
 
     DrawFPS(0, 0);
     EndDrawing();
