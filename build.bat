02130f4d4c0622f1e713cbff61987a82350b3e58
kxrosen <alxjbecg@gmail.com>
2025-04-10 17:20:22 +0700

    added player.cpp to the compilation

--- build.bat
+++ build.bat
@@ -1,1 +1,1 @@
-g++ main.cpp pong.cpp -o ouput -Wall -Wextra -Wconversion -O3 -I include\. -L lib\. -lraylib -lopengl32 -lgdi32 -lwinmm
+g++ main.cpp pong.cpp player.cpp -o ouput -Wall -Wextra -Wconversion -O3 -I include\. -L lib\. -lraylib -lopengl32 -lgdi32 -lwinmm
